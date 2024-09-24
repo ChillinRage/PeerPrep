59da149b0ac81f84ed8e841d8933b53916d7162a
ZENN\User <miljy123@gmail.com>
2024-09-24 17:07:08 +0800

    Implement text for problems page

--- frontend/src/pages/Problems.jsx
+++ frontend/src/pages/Problems.jsx
@@ -1,10 +1,14 @@
+import Navbar from "../components/Navbar";
 import QuestionTable from '../components/question-table'
+import '../styles/Problems.css';
 
 const Problems = () => {
     return (
         <div>
+        <Navbar />
         <h1>Problems</h1>
-        {/* <QuestionTable /> */}
+        <p className="description">View all the questions stored in database.</p>
+        <QuestionTable />
         </div>
     );
 }
