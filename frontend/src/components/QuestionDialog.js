59445deebe476bb6d43c3fcd9d11dc2c6a0a7b02
ZENN\User <miljy123@gmail.com>
2024-09-24 18:16:48 +0800

    Adjust formatting for questions dialog popup

--- frontend/src/components/QuestionDialog.js
+++ frontend/src/components/QuestionDialog.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, IconButton, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 const QuestionDialog = ({ open, question, onClose }) => {
     if (!question) return null;  // If no problem is selected, return null
 
     return (
         <Dialog open={open} onClose={onClose} scroll="paper" maxWidth="sm" fullWidth>
-            <DialogTitle>{question.title}</DialogTitle>
-            <DialogContent dividers>
-                <p><strong>Topic(s):</strong> {question.topic.join(', ')}</p>
-                <p><strong>Difficulty:</strong> {question.difficulty}</p>
+            <DialogTitle style={{color: 'black', fontSize: 36, fontFamily: 'Poppins', fontWeight: '800', wordWrap: 'break-word'}}>
+                {question.title}
+                <IconButton aria-label="close" onClick={onClose} style={{position: 'absolute', right: 8, top: 8}}>
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <Typography variant="body1" sx={{ marginBottom: 2, fontWeight: 'bold', fontFamily: 'Poppins' }}>
+                    <strong>Topic (s):</strong> {question.topic.join(', ')} &nbsp;&nbsp;&nbsp;
+                    <strong>Difficulty:</strong> {question.difficulty}
+                </Typography>
                 <p>{question.description}</p>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose} color="primary">
-                    Close
-                </Button>
-            </DialogActions>
         </Dialog>
     );
 };
