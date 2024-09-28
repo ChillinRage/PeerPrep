18e885f649471a5a186d9845e4db57ddbc29acfb
jannnice <e0969059@u.nus.edu>
2024-09-28 12:07:38 +0800

    Display Error Messages

--- frontend/src/components/CreateQuestionDialog.js
+++ frontend/src/components/CreateQuestionDialog.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem } from '@mui/material';
+import ErrorMessage from './ErrorMessageDialog'
 import questionService from '../services/question-service';
 import '../styles/create-question-dialog.css';
 
@@ -12,6 +13,8 @@ const difficulty_lvl = [
 const CreateQuestion = ({ open, handleClose }) => {
     const [difficulty, setDifficulty] = React.useState('Easy'); // Default difficulty
     const [imageFiles, setImageFiles] = React.useState([]); // State to hold image files
+    const [errorOpen, setErrorOpen] = React.useState(false); // State to control error dialog visibility
+    const [errorMessage, setErrorMessage] = React.useState(''); // State to store error message
 
     const handleDifficultyChange = (event) => {
         setDifficulty(event.target.value);
@@ -19,6 +22,10 @@ const CreateQuestion = ({ open, handleClose }) => {
 
     const handleImageFilesChange = (event) => {
         setImageFiles(event.target.files);
+    };
+
+    const handleErrorClose = () => {
+        setErrorOpen(false); // Close the error dialog
     };
 
     const handleSubmit = async (event) => {
@@ -52,127 +59,136 @@ const CreateQuestion = ({ open, handleClose }) => {
             window.location.reload();
             handleClose(); // Close the dialog after submission
         } catch (error) {
-            console.error('Error creating question:', error); // Handle the error appropriately
+            setErrorMessage('Error creating question: ' + error.message); // Set error message
+            setErrorOpen(true); // Open error dialog
         }
     };
 
     return (
-        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
-            <DialogTitle className="dialog-title">
-              Add New Question
-            </DialogTitle>
-            <form onSubmit={handleSubmit}>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        required
-                        margin="dense"
-                        id="title"
-                        name="title"
-                        label="Title"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                    <TextField
-                        margin="dense"
-                        required
-                        id="description"
-                        name="description"
-                        label="Description"
-                        type="text"
-                        fullWidth
-                        multiline
-                        rows={4}
-                        className="text-field"
-                    />
-                    <TextField
-                        margin="dense"
-                        required
-                        id="topic"
-                        name="topic"
-                        label="Topics (comma separated)"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                    <TextField
-                        margin="dense"
-                        fullWidth
-                        id="difficulty"
-                        select
-                        label="Difficulty Level"
-                        defaultValue="Easy"
-                        onChange={handleDifficultyChange}
-                        helperText="Select the difficulty level"
-                        className="text-field"
-                      >
-                        {difficulty_lvl.map((option) => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                        margin="dense"
-                        required
-                        id="input"
-                        name="input"
-                        label="Input"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                    <TextField
-                        margin="dense"
-                        required
-                        id="expected_output"
-                        name="expected_output"
-                        label="Expected Output"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                    <TextField
-                        margin="dense"
-                        id="images"
-                        name="images"
-                        label="Images URLs (comma separated)"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                    <input
-                        type="file"
-                        id="imageFiles"
-                        name="imageFiles"
-                        multiple
-                        accept="image/jpeg, image/jpg, image/png"
-                        onChange={handleImageFilesChange}
-                        className="file-input"
-                    />
-                    <TextField
-                        margin="dense"
-                        id="leetcode_link"
-                        name="leetcode_link"
-                        label="Leetcode Link"
-                        type="text"
-                        fullWidth
-                        multiline
-                        className="text-field"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} className="dialog-actions">Cancel</Button>
-                    <Button type="submit" className="dialog-actions">Add Question</Button>
-                </DialogActions>
-            </form>
-        </Dialog>
+        <>
+            <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+                <DialogTitle className="dialog-title">
+                Add New Question
+                </DialogTitle>
+                <form onSubmit={handleSubmit}>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            id="title"
+                            name="title"
+                            label="Title"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                        <TextField
+                            margin="dense"
+                            required
+                            id="description"
+                            name="description"
+                            label="Description"
+                            type="text"
+                            fullWidth
+                            multiline
+                            rows={4}
+                            className="text-field"
+                        />
+                        <TextField
+                            margin="dense"
+                            required
+                            id="topic"
+                            name="topic"
+                            label="Topics (comma separated)"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                        <TextField
+                            margin="dense"
+                            fullWidth
+                            id="difficulty"
+                            select
+                            label="Difficulty Level"
+                            defaultValue="Easy"
+                            onChange={handleDifficultyChange}
+                            helperText="Select the difficulty level"
+                            className="text-field"
+                        >
+                            {difficulty_lvl.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            margin="dense"
+                            required
+                            id="input"
+                            name="input"
+                            label="Input"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                        <TextField
+                            margin="dense"
+                            required
+                            id="expected_output"
+                            name="expected_output"
+                            label="Expected Output"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                        <TextField
+                            margin="dense"
+                            id="images"
+                            name="images"
+                            label="Images URLs (comma separated)"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                        <input
+                            type="file"
+                            id="imageFiles"
+                            name="imageFiles"
+                            multiple
+                            accept="image/jpeg, image/jpg, image/png"
+                            onChange={handleImageFilesChange}
+                            className="file-input"
+                        />
+                        <TextField
+                            margin="dense"
+                            id="leetcode_link"
+                            name="leetcode_link"
+                            label="Leetcode Link"
+                            type="text"
+                            fullWidth
+                            multiline
+                            className="text-field"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} className="dialog-actions">Cancel</Button>
+                        <Button type="submit" className="dialog-actions">Add Question</Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+
+            <ErrorMessage
+                open={errorOpen}
+                handleClose={handleErrorClose}
+                errorMessage={errorMessage}
+            />
+        </>
     );
 };
 
